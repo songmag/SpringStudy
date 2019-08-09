38decde62c6c92c2a8779bcb2c3f8d0f840113fc
songmag <songmag@naver.com>
2019-08-09 19:27:04 +0900

    mapper TestSource Make

--- Spring/WebContainer/src/test/java/com/spring/UserDAOTest.java
+++ Spring/WebContainer/src/test/java/com/spring/UserDAOTest.java
@@ -1,13 +1,16 @@
 package com.spring;
 
+
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
+
+import java.util.List;
 
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.SpringBootConfiguration;
+
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
@@ -18,14 +21,48 @@ import com.spring.model.UserVo;
 @ContextConfiguration(classes = WebContainerApplication.class)
 public class UserDAOTest {
 	@Autowired UserDAO userdao;
-	@Test
-	public void testAddUser()
+	UserVo user1,user2;
+	@Before
+	public void setUser()
 	{
-		
+		user1 = new UserVo();
+		user2 = new UserVo();
+		user1.setId("test1");
+		user1.setPw("1234");
+		user1.setName("test1");
+
+		user2.setId("test2");
+		user2.setPw("1234");
+		user2.setName("test2");
 	}
 	@Test
+	public void allTest()
+	{
+		testAddUser();
+		testSearch();
+		testDelete();
+	}
+	
+	public void testAddUser()
+	{
+		userdao.addUser(user1);
+		userdao.addUser(user2);
+	}
+	public void testSearch()
+	{
+		List<UserVo> users = userdao.getAll();
+		user1 = userdao.getOnce(user1);
+		user2 = userdao.getOnce(user2);
+		assertThat(users.size(),is(2));
+		assertThat(user1.getPhone_number(),is("000-000-0000"));
+		assertThat(user2.getPhone_number(),is("000-000-0000"));
+	}
 	public void testDelete()
 	{
+		userdao.deleteUser(user1);
+		userdao.deleteUser(user2);
+		List<UserVo> users = userdao.getAll();
+		assertThat(users.size(),is(0));
 	}
 	
 	//Test Scenario
