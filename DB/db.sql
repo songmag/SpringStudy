7050a2328ee88a0695e95e859b8515092a7dbb03
songmag <songmag@naver.com>
2019-08-10 18:12:36 +0900

    db query change and test view

--- DB/db.sql
+++ DB/db.sql
@@ -29,37 +29,22 @@ create table post
 	menu_num int not null,
 	post_name varchar(100) not null,
 	post_Date date default now(),
+	post_contents text default 'contents',
+	thumbnail varchar(255) default '/img/thumbnail.png',
+	
 	foreign key(id_num) references user_information(id_num) on delete cascade on update cascade,
 	foreign key(menu_num)
 	references menu(menu_num) on delete cascade on update cascade
-);
-create table context
-(
-	context_num int not null primary key auto_increment, 
-	post_num int not null,
-	id_num int not null,
-	menu_num int not null,
-	context text,
-	foreign key(id_num) references user_information(id_num) on delete cascade on update cascade,
-	foreign key(menu_num)
-	references menu(menu_num) on delete cascade on update cascade,
-	foreign key(post_num)
-	references post(post_num) on delete cascade on update cascade);
 
+);
 create table comment
 (
 	comment_num int not null primary key auto_increment,
-	context_num int not null, 
 	post_num int not null,
 	id_num int not null,
-	menu_num int not null,
 	comment_Date date default now(),
 	context text,
 	foreign key(id_num) references user_information(id_num) on delete cascade on update cascade,
-	foreign key(menu_num)
-	references menu(menu_num) on delete cascade on update cascade,
 	foreign key(post_num)
-	references post(post_num) on delete cascade on update cascade,
-	foreign key(context_num)
-	references context(context_num) on delete cascade on update cascade
+	references post(post_num) on delete cascade on update cascade
 );
